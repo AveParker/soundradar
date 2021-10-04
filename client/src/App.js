25858fc516f6eca87145b2ea499ee8522bff32b7
Tobias Müller <tobimandroid@googlemail.com>
2021-10-04 12:05:27 +0200

    added login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './Login';
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <Login />
 }
 
 export default App;
