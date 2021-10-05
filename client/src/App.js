c99994cf458a382ac54b9387258f3f82c9bcd1d0
Tobias Müller <tobimandroid@googlemail.com>
2021-10-05 11:29:26 +0200

    added refesh, session, token

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './App.css';
 import Login from './Login';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Dashboard from './Dashboard'
 
+const code = new URLSearchParams(window.location.search).get('code')
 
 function App() {
-  return <Login />
+  return code ? <Dashboard code = {code} /> : <Login />
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,16 +1,60 @@
-const app = require("./app");
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-});
+app.use(cors())
+app.use(bodyParser.json())
 
-var http = require("http");
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '6bf879ed4da54d7f8ba7511074c72f00',
+    clientSecret: 'ac03c02e869e4b49a61c0041d99fbe0e',
+    refreshToken
+  })
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+  spotifyApi.refreshAccessToken().then(
+    function(data) {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
+    })
+    .catch(() =>  {
+      res.sendStatus(400)
+    })
+})
+
+
+
+app.post('/login', (req,res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '6bf879ed4da54d7f8ba7511074c72f00',
+    clientSecret: 'ac03c02e869e4b49a61c0041d99fbe0e',
+  })
+
+
+   spotifyApi
+  .authorizationCodeGrant(code)
+  .then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
+    })
+  })
+  .catch(() => {
+    console.log(err)
+    res.sendStatus(400)
+  })
+})
+
+
+app.listen(3001)
