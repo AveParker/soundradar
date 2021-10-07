0630030244a817e8603c69c9279c57fc79b3129e
Tobias Müller <tobimandroid@googlemail.com>
2021-10-07 16:10:52 +0200

    added new chnages

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
+
+
 require("dotenv/config");
 
 // ℹ️ Connects to the database
@@ -18,6 +20,18 @@ require("./config")(app);
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
